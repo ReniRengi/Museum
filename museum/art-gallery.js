bf0718046f376a323d4de9f4d49a0bb43f0b8f48
Olga Bogdanova <TomieForever@gmail.com>
2021-09-24 19:59:52 +0300

    feat:js

---
+++ museum/art-gallery.js
@@ -0,0 +1,101 @@
+window.addEventListener('load', function (event) {
+      
+    loadImage();
+  });
+  const pictureInnerContainer = document.querySelector('.picture-inner-container');
+
+const img = document.createElement('img');
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery1.jpg`;
+img.alt = `galery1`;
+img.height=`456px`;
+pictureInnerContainer.append(img);
+
+const img2 = document.createElement('img');
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery2.jpg`;
+img.alt = `galery2`;
+img.height=`570px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery3.jpg`;
+img.alt = `galery3`;
+img.height=`570px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery4.jpg`;
+img.alt = `galery4`;
+img.height=`456px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery5.jpg`;
+img.alt = `galery5`;
+img.height=`570px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery6.jpg`;
+img.alt = `galery6`;
+img.height=`570px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery7.jpg`;
+img.alt = `galery7`;
+img.height=`570px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery8.jpg`;
+img.alt = `galery8`;
+img.height=`570px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery9.jpg`;
+img.alt = `galery9`;
+img.height=`570px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery10.jpg`;
+img.alt = `galery10`;
+img.height=`456px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery11.jpg`;
+img.alt = `galery11`;
+img.height=`456px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery12.jpg`;
+img.alt = `galery12`;
+img.height=`342px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery13.jpg`;
+img.alt = `galery13`;
+img.height=`342px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery14.jpg`;
+img.alt = `galery14`;
+img.height=`570px`;
+pictureInnerContainer.append(img);
+
+img.classList.add('gallery-img')
+img.src = `/assets/img/gallery/galery15.jpg`;
+img.alt = `galery15`;
+img.height=`456px`;
+pictureInnerContainer.append(img);
+
+
+
