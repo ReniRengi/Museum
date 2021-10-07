87b3ece8d02437f4b8e3daf1d6e9b148f523ad39
Olga Bogdanova <TomieForever@gmail.com>
2021-10-07 23:41:32 +0300

    feat js

--- museum-adaptive/js/form.js
+++ museum-adaptive/js/form.js
@@ -1,3 +1,9 @@
+export function initForm() {
+  window.addEventListener('load', function (event) {
+    ///loadForm();
+    createRipple();
+  });
+
 function loadForm() {
   let modal = document.getElementById('byTicketModal');
   let btn = document.getElementById('total-button');
@@ -34,9 +40,25 @@ for (const button of buttons) {
   button.addEventListener('click', createRipple);
 }
 
-export function initForm() {
-  window.addEventListener('load', function (event) {
-    ///loadForm();
-    createRipple();
-  });
+const form=document.getElementById("ticket-form");
+const nameP=document.getElementById("name");
+const email=document.getElementById("email");
+const date=document.getElementById("date");
+const time=document.getElementById("time");
+const phone=document.getElementById("phone");
+const ticketType=document.getElementById("ticket-type");
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+});
+
+const isRequired = value => value === '' ? false : true; // проверка значения на пустоту;
+
+const isBetween = (length, min, max) => length < min || length > max ? false : true;//проверка на длинуж
+
+const isNameValid = (nameP) => {
+  
 }
+
+
