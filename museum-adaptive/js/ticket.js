438ec3bb2f9bcecca1489f3fa1b72e2d75811fd9
Olga Bogdanova <TomieForever@gmail.com>
2021-10-08 16:13:31 +0300

    feat:hyh

--- museum-adaptive/js/ticket.js
+++ museum-adaptive/js/ticket.js
@@ -8,14 +8,15 @@ export function initTicket() {
       let item;
       let value1;
       let value2;
+      console.log(document.getElementById('price'));
     form.addEventListener("click", function(event) {
        item=form.querySelector('input[name="radio"]:checked').value;
        value1=document.getElementById('num1').value;
        value2=document.getElementById('num2').value;
        price=document.getElementById('price');
-       /*total=(item*value1)+((item/2)*value2);*/
-       console.log(total);
-       let updatePrice = (value1, value2, item) => price.innerHTML =`{(item*value1)+((item/2)*value2)}`;
+       total=(item*value1)+((item/2)*value2);
+       console.log(price);
+       let updatePrice = (total) => price.textContent =total;
        updatePrice();
       });
     
